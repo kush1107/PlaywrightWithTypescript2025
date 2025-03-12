46ac9b6e730634cb43bcf7e32eddbbf5587eb29a
kush1107 <kushalparikh123@gmail.com>
2025-03-12 10:59:28 +0530

    Playwright Test Automation with Typescript 2025

--- tests/HooksTest2.spec.ts
+++ tests/HooksTest2.spec.ts
@@ -5,6 +5,7 @@ let page;
 test.beforeAll(async ({browser}) => {
     page = await browser.newPage();
     await page.goto("https://www.saucedemo.com/"); // Navigate to URL
+    await page.waitForLoadState('domcontentloaded');
     await expect(page).toHaveTitle('Swag Labs'); // Wait till title appears
     await page.locator("#user-name").fill("standard_user"); // Enter username
     await page.locator("#password").fill("secret_sauce");  // Enter password
@@ -14,14 +15,17 @@ test.beforeAll(async ({browser}) => {
 
 test.afterAll(async () => {
     console.log("Logout from the Application");
+    await page.locator("//button[@id='react-burger-menu-btn']").click();
+    await page.locator("//a[@id='logout_sidebar_link']").click();
+    await page.close();
     
 });
 
 
     test("Adding Product -1 to Cart & Checkout ", async () => {
-        console.log("Entering Credentials to Login Form - Valid");
+        console.log("Adding Product -1 to Cart & Checkout");
         await page.locator("//a[normalize-space()='Sauce Labs Backpack']").click();
-       await page.locator("#add-to-cart",{ state: 'attached' }).click();
+        await page.locator("#add-to-cart",{ state: 'attached' }).click();
         await page.locator(".shopping_cart_link").click();
         await page.locator("#checkout").click();
         await page.locator("//input[@id='first-name']").fill("Kushal");
@@ -32,23 +36,35 @@ test.afterAll(async () => {
         await expect(page.locator("//h2[normalize-space()='Thank you for your order!']")).toHaveText("Thank you for your order!");
     });
 
-    // test("Login to App Using InValid Credentials", async () => {
-    //     console.log("Entering Credentials to Login Form - Invalid");
-    //     await page.locator("#user-name").fill("standard_user12"); // Enter invalid username
-    //     await page.locator("#password").fill("secret_sauce12");  // Enter invalid password
-    //     await page.locator("#login-button").click(); // Click the login button
-    //     await expect(page.locator("[data-test='error']")).toHaveText(
-    //         "Epic sadface: Username and password do not match any user in this service"
-    //     ); // Verify error message
-    // });
+    test("Adding Product -2 to Cart & Checkout ", async () => {
+        await page.goto("https://www.saucedemo.com/inventory.html"); // Navigate to URL
+        await page.waitForLoadState('domcontentloaded');
+        console.log("Adding Product -2 to Cart & Checkout");
+        await page.locator("//a[normalize-space()='Sauce Labs Fleece Jacket']").click();
+        await page.locator("#add-to-cart",{ state: 'attached' }).click();
+        await page.locator(".shopping_cart_link").click();
+        await page.locator("#checkout").click();
+        await page.locator("//input[@id='first-name']").fill("Kushal");
+        await page.locator("//input[@id='last-name']").fill("Parikh");
+        await page.locator("#postal-code").fill("390001");
+        await page.locator("#continue").click();
+        await page.locator("#finish").click();
+        await expect(page.locator("//h2[normalize-space()='Thank you for your order!']")).toHaveText("Thank you for your order!");
+    });
 
-    // test("Login to App Using Empty Credentials", async () => {
-    //     console.log("Entering Credentials to Login Form - Empty");
-    //     await page.locator("#user-name").fill(""); // Leave username empty
-    //     await page.locator("#password").fill("");  // Leave password empty
-    //     await page.locator("#login-button").click(); // Click the login button
-    //     await expect(page.locator("[data-test='error']")).toHaveText(
-    //         "Epic sadface: Username is required"
-    //     ); // Verify error message
-    // });
+    test("Adding Product -3 to Cart & Checkout ", async () => {
+        await page.goto("https://www.saucedemo.com/inventory.html"); // Navigate to URL
+        await page.waitForLoadState('domcontentloaded');
+        console.log("Adding Product -3 to Cart & Checkout");
+        await page.locator("//a[normalize-space()='Sauce Labs Bolt T-Shirt']").click();
+        await page.locator("#add-to-cart",{ state: 'attached' }).click();
+        await page.locator(".shopping_cart_link").click();
+        await page.locator("#checkout").click();
+        await page.locator("//input[@id='first-name']").fill("Kushal");
+        await page.locator("//input[@id='last-name']").fill("Parikh");
+        await page.locator("#postal-code").fill("390001");
+        await page.locator("#continue").click();
+        await page.locator("#finish").click();
+        await expect(page.locator("//h2[normalize-space()='Thank you for your order!']")).toHaveText("Thank you for your order!");
+    });
 
