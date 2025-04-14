f4108734e13386f41688f9e857f415fa3ea9dcc5
Kush <kushalparikh123@gmail.com>
2025-04-15 00:42:53 +0530

    Iframe - Playwright

--- tests/playwright_Tutorials2025/Frames_test.spec.ts
+++ tests/playwright_Tutorials2025/Frames_test.spec.ts
@@ -5,12 +5,16 @@ test("Test to handle iFrame",async ({page})=>{
     await page.goto('https://letcode.in/frame'); // navigating to url
     await page.waitForLoadState('domcontentloaded'); //waiting for DOM to get loaded
 
+    const all_frames =  page.frames();
+    console.log("No. of frames in page:"+all_frames.length);
     // Locate the iframe and get its frame
-    const frameElement = await page.frameLocator("#firstFr");
+    const frameElement = page.frameLocator("#firstFr");
 
     // Interact with the input field inside the iframe
     await frameElement.locator("input[placeholder='Enter name']").fill('Kushal');
-    await frameElement.locator("input[placeholder='Enter email']").fill('Parikh');;
+    await frameElement.locator("input[placeholder='Enter email']").fill('Parikh');
    
+    //Interact with frames in nested iframe
+    await frameElement.frameLocator("iframe[src='innerframe']").locator("input[name='email']").fill('kparikh123@gmail.com');
 
 })
